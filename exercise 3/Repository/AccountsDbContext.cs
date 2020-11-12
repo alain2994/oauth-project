dba90d8d7d65b4321fa6651c7f8511e4d40dc610
charbelh3 <charbel.elhelou2000@gmail.com>
2020-11-12 12:59:11 +0200

    Add data access layer

---
+++ exercise 3/Repository/AccountsDbContext.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using exercise_3.Repository.Models;
+
+namespace exercise_3.Repository
+{
+    public partial class AccountsDbContext : DbContext
+    {
+        public AccountsDbContext()
+        {
+        }
+
+        public AccountsDbContext(DbContextOptions<AccountsDbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Account> Accounts { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Data Source=LAPTOP-PTFRNTAK\\SQLEXPRESS2019;Initial Catalog=AccountsDatabase;Integrated Security=True; MultipleActiveResultSets=True");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Account>(entity =>
+            {
+                entity.Property(e => e.Address).IsUnicode(false);
+
+                entity.Property(e => e.Email).IsUnicode(false);
+
+                entity.Property(e => e.FirstName).IsUnicode(false);
+
+                entity.Property(e => e.LastName).IsUnicode(false);
+
+                entity.Property(e => e.Password).IsUnicode(false);
+
+                entity.Property(e => e.Username).IsUnicode(false);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ exercise 3/Repository/Models/Account.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace exercise_3.Repository.Models
+{
+    [Index(nameof(Username), Name = "UQ__Accounts__F3DBC572DAB2D3CE", IsUnique = true)]
+    public partial class Account
+    {
+        [Key]
+        [Column("id")]
+        public int Id { get; set; }
+        [Required]
+        [Column("first_name")]
+        [StringLength(30)]
+        public string FirstName { get; set; }
+        [Required]
+        [Column("last_name")]
+        [StringLength(30)]
+        public string LastName { get; set; }
+        [Required]
+        [Column("email")]
+        [StringLength(30)]
+        public string Email { get; set; }
+        [Column("address")]
+        [StringLength(30)]
+        public string Address { get; set; }
+        [Required]
+        [Column("username")]
+        [StringLength(30)]
+        public string Username { get; set; }
+        [Required]
+        [Column("password")]
+        public string Password { get; set; }
+    }
+}
