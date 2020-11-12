3df4bd166b32edcf56043743b7fcf001fcb68b21
charbelh3 <charbel.elhelou2000@gmail.com>
2020-11-12 14:08:21 +0200

    Add response model for Account table

---
+++ exercise 3/ResponseModels/ResponseAccount.cs
@@ -0,0 +1,33 @@
+﻿using exercice_2.IMWFramework.ResponseModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace exercise_3.ResponseModels
+{
+    public class ResponseAccount:ResponseModel
+    {
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
+
+        [JsonPropertyName("first_name")]
+        public string FirstName { get; set; }
+
+        [JsonPropertyName("last_name")]
+        public string LastName { get; set; }
+
+        [JsonPropertyName("email")]
+        public string Email { get; set; }
+
+        [JsonPropertyName("address")]
+        public string Address { get; set; }
+
+        [JsonPropertyName("username")]
+        public string Username { get; set; }
+
+        [JsonPropertyName("password")]
+        public string Password { get; set; }
+    }
+}
